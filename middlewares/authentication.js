f6f24edf639698c85494a54fb384f2037eb99402
Binod Raj Dhami <binodraazdhami@gmail.com>
2024-08-29 04:26:19 +0545

    add authentication middleware

---
+++ middlewares/authentication.js
@@ -0,0 +1,53 @@
+const jwt = require('jsonwebtoken');
+const configs = require('./../configs');
+const prisma = require("./../configs/prisma.client");
+
+module.exports = function (req, res, next) {
+    let token;
+    if (req.headers['x-access-token']) {
+        token = req.headers['x-access-token'];
+    }
+    if (req.headers['authorization']) {
+        token = req.headers['authorization'];
+    }
+    if (req.headers.token) {
+        token = req.headers['authorization'];
+    }
+    if (req.query.token) {
+        token = req.query['token'];
+    }
+    if (token) {
+        jwt.verify(token, configs.app.jwtSecret, async function (err, verifiedUser) {
+            if (err) {
+                return next(err);
+            }
+            if (verifiedUser) {
+                const user = await prisma.user.findUnique({
+                    where: {
+                        id: verifiedUser.id
+                    }
+                });
+
+                if (!user) {
+                    return next({
+                        status: 404,
+                        msg: 'User not found'
+                    });
+                }
+
+                req.loggedInUser = user;
+                next();
+
+            } else {
+                next({
+                    status: 400,
+                    msg: 'Token expired or verification failed'
+                });
+            }
+        });
+    } else {
+        res.status(400).json({
+            msg: 'Token not provided'
+        });
+    }
+}
--- routes/index.js
+++ routes/index.js
@@ -1,7 +1,8 @@
 const express = require('express');
 const router = express.Router();
+const authentication = require('./../middlewares/authentication');
 
 router.use('/auth', require('./auth.routes'));
-router.use('/users', require('./user.routes'));
+router.use('/users', authentication, require('./user.routes'));
 
 module.exports = router;
